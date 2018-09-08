4c6a43271807fd00f52ac6b556cb9f6b5c1878e2
JallenKwong <jallenkwong@163.com>
2018-09-08 17:58:10 +0800

    Merge Subtitles Of Images

---
+++ MergeSubtitlesOfImages/README.md
@@ -0,0 +1,11 @@
+# 截图字幕叠加 #
+
+## 步骤 ##
+
+- Python 2.7
+- pip install pillow
+- 在工作目录创建名为image的文件夹，将截图们放入该文件夹
+- 运行main.py脚本
+- 工作目录内会创建结果图片文件result.jpg
+
+![](result.jpg)
---
+++ MergeSubtitlesOfImages/main.py
@@ -0,0 +1,54 @@
+# -*- coding: UTF-8 -*-
+
+# 视频截图字幕缩叠
+# 作者：白居布衣
+
+from PIL import Image
+import os
+
+folderName = "image\\"
+
+if os.path.exists(folderName) is False:
+    print "image folder is not existed."
+    exit()
+
+imagenamelist = os.listdir(folderName)
+
+if len(imagenamelist) <= 1:
+    print "There is not enough images in image folder. At least 2 images"
+    exit()
+
+firstImage = Image.open(folderName + imagenamelist[0])
+width, height = firstImage.size # 
+
+# 底部字幕的高度 ，所以图片要截取的位置是(0, 400 - 55 + 1,720 , 400)
+cropHeight = 55
+
+imagelist = []
+imagelist.append(firstImage)
+#print firstImage.size
+
+#截取各图
+for imagename in imagenamelist:
+    if imagenamelist.index(imagename) == 0:
+        continue
+    im = Image.open(folderName + imagename)
+    imagelist.append(im.crop((0, height - cropHeight, width, height)))
+        
+#开始合并
+
+#先计算高度
+newImageHeight = height + (len(imagenamelist) - 1) * cropHeight
+newImage = Image.new('RGB', (width, newImageHeight))
+count = 0
+
+for im in imagelist:    
+    if imagelist.index(im) == 0:
+        newImage.paste(im.copy(),(0, 0))
+    else:
+        newImage.paste(im.copy(),(0, count))
+    count = count + im.size[1]
+
+newImage.save('result.jpg')
+
+print 'Done!'
