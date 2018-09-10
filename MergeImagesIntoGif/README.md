8b6155c45423f4f9e123e94d7d934e69d73dd593
JallenKwong <jallenkwong@163.com>
2018-09-10 10:18:53 +0800

    Merge Images Into Gif

---
+++ MergeImagesIntoGif/README.md
@@ -0,0 +1,10 @@
+# 图片们合成GIF #
+
+## 步骤 ##
+
+- Python 2.7
+- pip install imageio
+- 在工作目录创建名为image的文件夹，将图片们放入该文件夹
+- 运行main.py脚本
+- 工作目录内会创建结果文件result.gif
+
---
+++ MergeImagesIntoGif/main.py
@@ -0,0 +1,30 @@
+# -*- coding: UTF-8 -*-
+
+# 图片集弄成gif文件
+# 作者：白居布衣
+
+import imageio
+import os
+
+folderName = "image\\"
+
+if os.path.exists(folderName) is False:
+    print "image folder is not existed."
+    exit()
+
+imagenamelist = os.listdir(folderName)
+
+if len(imagenamelist) <= 1:
+    print "There is not enough images in image folder. At least 2 images"
+    exit()
+
+frames = []
+duration = 3
+
+for imagename in imagenamelist:
+    
+    frames.append(imageio.imread(folderName + imagename))
+# 保存为 gif 
+imageio.mimsave("result.gif", frames, 'GIF', duration = duration)
+
+print "Done"
